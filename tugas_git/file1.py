f40a7bbc7b8bcc77015bcab17cddf45648b85477
ghalitsardev <ghalitsardev@gmail.com>
2024-12-02 11:17:04 +0700

    Menambahkan Output identitas nomor

--- tugas_git/file1.py
+++ tugas_git/file1.py
@@ -23,28 +23,39 @@ data_panen = {
 
 
 def harvest_list(data):
+    print("Nomor 1")
     [
         print(
-            f"Lokasi {value['nama_lokasi']} Hasil panen\nJagung {value['hasil_panen']['jagung']}\nPadi {value['hasil_panen']['padi']}\nKedelai {value['hasil_panen']['kedelai']} {'\n'*2} "
+            f"Lokasi {value['nama_lokasi']} Hasil panen\nJagung {value['hasil_panen']['jagung']}\nPadi {value['hasil_panen']['padi']}\nKedelai {value['hasil_panen']['kedelai']} {'\n'} "
         )
         for value in data.values()
     ]
+    print("\nNomor 2")
+    
     [
         print(
             f"Lokasi {value['nama_lokasi']} Hasil panen\nJagung {value['hasil_panen']['jagung']}{'\n'*2} "
         )
         for value in data.values()
     ]
+    print("\nNomor 3")
+    
     [
         print(f"Nama Lokasi dari {key} adalah {value['nama_lokasi']} \n\n")
         for key, value in data.items()
         if key == "lokasi3"
     ]
+    print("Nomor 4")
+    
     rice_fields_sum = sum(val["hasil_panen"]["padi"] for val in data.values())
     soybean_fields_sum = sum(val["hasil_panen"]["kedelai"] for val in data.values())
     print(f"Hasil panen Padi : {rice_fields_sum} , Kedelai : {soybean_fields_sum}")
     
-    [{key:{'padi':val['hasil_panen']['padi'],'kedelai':val['hasil_panen']['kedelai']}}  for key, val in data.items()]
+    print("\nNomor 5")
+    dataVar = [{key:{'padi':val['hasil_panen']['padi'],'kedelai':val['hasil_panen']['kedelai']}}  for key, val in data.items()]
+    print(f"Variable Baru {dataVar}")
+
+    print("\n\nNomor 6")
     [print(f"Lokasi {val['nama_lokasi']} memerlukan perhatian khusus") if val['hasil_panen']['padi'] > 1300 or val['hasil_panen']['jagung'] > 800 else print(f"Lokasi {val['nama_lokasi']} dalam kondisi baik ") for key, val in data.items()]
 
 
