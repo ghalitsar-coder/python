00d1b166558ee1f5e2e12a518730b5ccd8060b0d
ghalitsardev <ghalitsardev@gmail.com>
2024-12-02 13:07:54 +0700

    pengubahan nomor 5

--- tugas_git/file1.py
+++ tugas_git/file1.py
@@ -24,40 +24,26 @@ data_panen = {
 
 def harvest_list(data):
     """Fungsi untuk melihat hasil panen"""
-    print("Nomor 1")
-    [
-        print(
-            f"Lokasi {value['nama_lokasi']} Hasil panen\nJagung {value['hasil_panen']['jagung']}\nPadi {value['hasil_panen']['padi']}\nKedelai {value['hasil_panen']['kedelai']} {'\n'} "
-        )
-        for value in data.values()
-    ]
-    print("\nNomor 2")
+    print(" ======== Nomor 1 ===========")
+    [print( f"Lokasi {value['nama_lokasi']} Hasil panen\nJagung {value['hasil_panen']['jagung']}\nPadi {value['hasil_panen']['padi']}\nKedelai {value['hasil_panen']['kedelai']} {'\n'} ") for value in data.values()]
     
-    [
-        print(
-            f"Lokasi {value['nama_lokasi']} Hasil panen\nJagung {value['hasil_panen']['jagung']}{'\n'*2} "
-        )
-        for value in data.values()
-    ]
-    print("\nNomor 3")
+    print("\n ======== Nomor 2 ===========")
+    [print(f"Lokasi {value['nama_lokasi']} Hasil panen\nJagung {value['hasil_panen']['jagung']}{'\n'*1} ")for value in data.values()]
     
-    [
-        print(f"Nama Lokasi dari {key} adalah {value['nama_lokasi']} \n\n")
-        for key, value in data.items()
-        if key == "lokasi3"
-    ]
-    print("Nomor 4")
+    print("\n ======== Nomor 3 ===========")
+    [print(f"Nama Lokasi dari {key} adalah {value['nama_lokasi']}\n")for key, value in data.items()if key == "lokasi3"]
     
+    print(" ======== Nomor 4 ===========")
     rice_fields_sum = sum(val["hasil_panen"]["padi"] for val in data.values())
     soybean_fields_sum = sum(val["hasil_panen"]["kedelai"] for val in data.values())
     print(f"Hasil panen Padi : {rice_fields_sum} , Kedelai : {soybean_fields_sum}")
     
-    print("\nNomor 5")
-    dataVar = [{key:{'padi':val['hasil_panen']['padi'],'kedelai':val['hasil_panen']['kedelai']}}  for key, val in data.items()]
-    print(f"Variable Baru {dataVar}")
+    print("\n ======== Nomor 5 ===========")
+    dataVar = {val['nama_lokasi']: {'padi': val['hasil_panen']['padi'],'kedelai': val['hasil_panen']['kedelai'],'jagung': val['hasil_panen']['jagung']}for val in data.values()}
+    [print(f"Lokasi {key}\nPadi:{val['padi']}\nKedelai:{val['kedelai']}\nJagung:{val['jagung']}\n ")  for key,val in dataVar.items()]
 
-    print("\n\n\nNomor 6")
-    [print(f"Lokasi {val['nama_lokasi']} memerlukan perhatian khusus") if val['hasil_panen']['padi'] > 1300 or val['hasil_panen']['jagung'] > 800 else print(f"Lokasi {val['nama_lokasi']} dalam kondisi baik ") for key, val in data.items()]
+    print("\n\n\n ======== Nomor 6 ===========")
+    [print(f"Lokasi {key} memerlukan perhatian khusus") if val['padi'] > 1300 or val['jagung'] > 800 else print(f"Lokasi {key} dalam kondisi baik ") for key, val in dataVar.items()]
 
 
 harvest_list(data_panen)
